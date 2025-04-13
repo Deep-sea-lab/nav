6e97142dd33a411df39419156d0fb2d2a2723d2c
l cx <3800907852@qq.com>
2025-04-13 18:07:27 +0800

    first

---
+++ src/components/AllFunc/AllBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <n-tabs class="all-box" size="large" justify-content="space-evenly" animated>
+    <n-tab-pane class="no-padding height--full" name="link" tab="捷径">
+      <ShortCut />
+    </n-tab-pane>
+  </n-tabs>
+</template>
+
+<script setup>
+import { NTabs, NTabPane } from "naive-ui";
+import ShortCut from "@/components/AllFunc/Box/ShortCut.vue";
+</script>
+<style>
+.height--full {
+  height: 100%;
+}
+</style>
